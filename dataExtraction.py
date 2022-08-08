ed69c5abf35e336195e3dcf7b58ada06771456d1
Monokoa Kolotsane <kmonokoa5@gmail.com>
2022-08-08 09:30:42 +0200

    updated

--- dataExtraction.py
+++ dataExtraction.py
@@ -1,28 +1,49 @@
 import PyPDF2
 import os
+import re
+import openpyxl
 
-dir_path = 'C://Users//KOLOTSANE//PycharmProjects//DataExtraction//invoices//'
-os.chdir(dir_path)
-file_list = os.listdir()
+excelsheet = openpyxl.load_workbook('Invoice_Information.xlsx')
+sheet = excelsheet['Sheet1']
 
-for file_Name in file_list:
+for file_name in os.listdir('invoices'):
+    print(file_name)
+    load_pdf = open(r'C:\\Users\\KOLOTSANE\\PycharmProjects\\DataExtraction\\invoices\\' + file_name, 'rb')
+    read_pdf = PyPDF2.PdfFileReader(load_pdf, strict=False)
+    page_count = read_pdf.getNumPages()
+    first_page = read_pdf.getPage(0)
+    page_content = first_page.extractText()
+    # print(page_content)
 
-    f = open(dir_path + file_Name, 'rb')
-    reader = PyPDF2.PdfFileReader(f)
-    file_contents = reader.getPage(0).extract_text().split("\n")
+    try:
+        # company_name = re.search(r'Company(.*)', page_content).group().split("Company")[1]
+        # print(company_name)
+        # address = invoice_no = re.search(r'Address(.*)', page_content).group().split("Address")[1]
+        # print(address)
+        # email = re.search(r'([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9._-]+)', page_content).group()
+        # print(email)
+        invoice_no = re.search(r'Invoice Number(.*)', page_content).group().split("Invoice Number")[1]
+        print(invoice_no)
+        invoice_date = re.search(r'Invoice Date (.*)', page_content).group().split("Invoice Date")[1]
+        print(invoice_date)
+        due_date = re.search(r'Due Date (.*)', page_content).group().split("Due Date")[1]
+        print(due_date)
+        sub_total = re.search(r'Sub Total (.*)', page_content).group().split("Sub Total")[1]
+        print(sub_total)
+        total = re.search(r'Total Due (.*)', page_content).group().split("Total Due")[1]
+        print(total)
 
-    invoice_no = ""
-    date = ""
+        last_row_number = sheet.max_row
+        print(last_row_number)
 
-    for i in range(len(file_contents)):
-        if file_contents[i].find('INVOICE #') != -1:
-            invoice_no = file_contents[i].split(" ")[1]
-        if file_contents[i].find('DATE') != -1:
-            date = file_contents[i].split(":")[1]
-        if file_contents[i].find('Invoice Number') != -1:
-            invoice_no = file_contents[i].split(" ")[1]
-        if file_contents[i].find('Invoice Date') != -1:
-            date = file_contents[i].split(" ")[1]
+        sheet.cell(column=1, row=last_row_number + 1).value = invoice_no
+        sheet.cell(column=2, row=last_row_number + 1).value = invoice_date
+        sheet.cell(column=3, row=last_row_number + 1).value = due_date
+        sheet.cell(column=4, row=last_row_number + 1).value = sub_total
+        sheet.cell(column=5, row=last_row_number + 1).value = total
 
-    print(invoice_no + ',' + date)
-    f.close()
+        # saving a file
+        excelsheet.save('Invoice_Information.xlsx')
+    except:
+        print("Process failed")
+
